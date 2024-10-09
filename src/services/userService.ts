7f405bb04db7495524b2f7cffa5a9289d8dfac40
Shalom Raichman <shalomr1770@gmail.com>
2024-10-09 15:14:47 +0300

    add service func for adding a user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,11 @@
-import User from "../modeles/user"
+import User from "../modeles/interfases/IUser"
+import { UserModel } from "../modeles/schemas/userSchema"
 
-export const createUser = async (user: User): Promise<void> =>{
-    
+export const createUser = async (user: User): Promise<void> => {
+    try {
+        const dbUsur = new UserModel({ user_name: user.user_name })
+        await dbUsur.save()
+    } catch (err) {
+        console.log(err)
+    }
 }
-
