2e5d1b31df11f931cec233ff663211a9a0f899a5
Shalom Raichman <shalomr1770@gmail.com>
2024-10-09 11:15:02 +0300

    connect to mongo

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import exp, { Express } from "express"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/userRouter"
 import "dotenv/config"
+import { connectToMongoos } from "./config/dbConfig"
 
 const PORT = process.env.PORT || 1415
 
@@ -9,6 +10,8 @@ const app: Express = exp()
 
 app.use(exp.json())
 app.use(cookieParser())
+
+connectToMongoos()
 
 app.use("/user", userRouter)
 
