df0bcc9a2c6dcdb0131d14b3b1171b5465be6939
Shalom Raichman <shalomr1770@gmail.com>
2024-10-09 10:59:48 +0300

    remove unused import

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose"
-import mongo from "mongoose"
 
 export const connectToMongoos = async (): Promise<void> => {
     try {
         await mongoose.connect("mongodb://localhost:27017/todos")
-        console.log("Successfuly connected to mongo");
+        console.log("Successfuly connected to mongo")
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
 }
